f91a313d1eead0fbd6c574270c63cf1d665f0862
Bernhard Urban <lewurm@gmx.net>
2009-08-17 03:58:21 +0200

    added usb/include/usb.h (from linux/include/linux/usb.h)

--- main.c
+++ main.c
@@ -29,6 +29,7 @@ Copyright (C) 2009		John Kelley <wiidev@kelley.ca>
 #include "nand.h"
 #include "boot2.h"
 #include "git_version.h"
+#include "usb/include/usb.h"
 #include "usb/core/hcd.h"
 #include "usb/host/ohci.h"
 
